8eb0e595ae3c4f90d1ef53b0832e0db9a1896922
Nidhish Adhikari <nadhikari4@myseneca.ca>
2026-06-04 16:27:18 -0400

    completed lab1

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program performs a math calculation using objects.'''
+
+x = 10
+y = 2
+z = 5
+
+answer = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(answer))
